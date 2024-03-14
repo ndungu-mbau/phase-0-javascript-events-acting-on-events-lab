cf21bc7965aeb567bb8c0efd057ce820439cbb41
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-14 09:09:48 +0300

    feat: add moving functions, attach event listener

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace('px', '')
+  const left = parseInt(leftNumbers, 10)
+
+  dodger.style.left = `${left - 1}px`
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '')
+  const left = parseInt(leftNumbers, 10)
+
+  dodger.style.left = `${left + 1}px`
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
